605d2d01bb841da0751c03f1b9b0eb9f063f4035
hamza19mughal99 <57133610+hamza19mughal99@users.noreply.github.com>
2021-05-12 00:41:12 +0500

    Initial commit

---
+++ flutter/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Hello World',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter First Project'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({@required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(
+          'Hamza Ahmed Mughal    hamza19mughal99@gmail.com',
+        ),
+      ),
+    );
+  }
+}
